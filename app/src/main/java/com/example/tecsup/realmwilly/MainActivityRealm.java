327985d62c98eac0fc862d253e935c6287e1abdc
willy <wbps85@gmail.com>
2019-05-06 21:58:41 -0500

    actualizando el proyecto

--- app/src/main/java/com/example/tecsup/realmwilly/MainActivityRealm.java
+++ app/src/main/java/com/example/tecsup/realmwilly/MainActivityRealm.java
@@ -6,8 +6,10 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -50,10 +52,20 @@ public class MainActivityRealm extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Nuevo Libro");
         builder.setMessage("Ingrese los datos para un nuevo libro");
+        LayoutInflater inflater = LayoutInflater.from(this);
+        //inflando la vista
+        View v = inflater.inflate(R.layout.agregar_libro, null);
+        //Creo los textview
+        final TextView titulo = v.findViewById(R.id.titulo);
+        final TextView autor = v.findViewById(R.id.autor);
+        builder.setView(v);
         builder.setPositiveButton("Agregar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                Libros l =new Libros(titulo.getText().toString(), autor.getText().toString());
+                realm.beginTransaction();
+                realm.copyToRealm(l);
+                realm.commitTransaction();
             }
         });
         builder.show();
